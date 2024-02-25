40be03d731e254ab48c16201eb6cd1daa366ebe6
William Hart <hart.wl@gmail.com>
2024-02-25 17:39:55 +1100

    Limit the number of counters that can be created on the server

--- src/routes.rs
+++ src/routes.rs
@@ -1,15 +1,20 @@
+use askama::Template;
+use askama_axum::IntoResponse;
 use axum::{
     extract::{Path, State},
     http::StatusCode,
 };
 
 use crate::{
-    components::{
-        self,
-        counter::{self, Counter},
-    },
+    components::{self, counter},
     state::AppState,
 };
+
+#[derive(Template)]
+#[template(path = "error.html")]
+pub struct ErrorResponse {
+    error: String,
+}
 
 pub(crate) async fn home_route_handler(
     mut state: State<AppState>,
@@ -18,8 +23,19 @@ pub(crate) async fn home_route_handler(
     crate::components::pages::index(counters)
 }
 
-pub(crate) async fn add_counter_mutation(mut state: State<AppState>) -> Counter {
-    state.create_counter().await
+pub(crate) async fn add_counter_mutation(mut state: State<AppState>) -> impl IntoResponse {
+    // do an expensive check to see if we have >= 100 counters.
+    if state.get_counters().await.len() >= 5 {
+        return (
+            StatusCode::OK,
+            ErrorResponse {
+                error: "Too many counters, can't create a new counter".to_owned(),
+            },
+        )
+            .into_response();
+    }
+
+    (StatusCode::OK, state.create_counter().await).into_response()
 }
 
 pub(crate) async fn delete_counter_mutation(
